6206314dbb08f7c53e2eb99056f9eb97a2f2bd73
John Lennon Sousa Costa <lennonsousa@outlook.com>
2022-05-18 17:34:45 -0300

    test: ensure password is required

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -7,7 +7,7 @@ describe("SignUp Controller", () => {
 
     const httpRequest = {
       body: {
-        email: "any_name",
+        email: "any@email.com",
         password: "any_password",
         passwordConfirmation: "any_password",
       },
@@ -32,5 +32,21 @@ describe("SignUp Controller", () => {
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("email"));
+  });
+
+  test("Should return 400 if no password is provided", () => {
+    const sut = new SignUpController();
+
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any@email.com",
+        passwordConfirmation: "any_password",
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("password"));
   });
 });
--- src/presentation/protocols/http.ts
+++ src/presentation/protocols/http.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 export interface HttpResponse {
   statusCode: number;
-  body: any;
+  body: unknown;
 }
 
 export interface HttpRequest {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   body?: any;
 }
