4d8cd9945be5248ce1fcd773d39ce313155254f0
John Lennon Sousa Costa <lennonsousa@outlook.com>
2022-06-13 14:42:05 -0300

    feat: ensure signup controller returns 500 if email validadtor throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,32 +3,40 @@ import { Controller } from "../protocols/controller";
 import { EmailValidator } from "../protocols/emailValidator";
 import { InvalidParamError } from "../protocols/errors/invalidParamError";
 import { MissingParamError } from "../protocols/errors/missingParamError";
+import { ServerError } from "../protocols/errors/serverError";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 
 export class SignUpController implements Controller {
   constructor(private readonly emailValidator: EmailValidator) {}
 
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = [
-      "name",
-      "email",
-      "password",
-      "passwordConfirmation",
-    ];
+    try {
+      const requiredFields = [
+        "name",
+        "email",
+        "password",
+        "passwordConfirmation",
+      ];
 
-    // eslint-disable-next-line no-restricted-syntax
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field])
-        return badRequest(new MissingParamError(field));
+      // eslint-disable-next-line no-restricted-syntax
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field])
+          return badRequest(new MissingParamError(field));
+      }
+
+      const isValidEmail = this.emailValidator.isValid(httpRequest.body.email);
+
+      if (!isValidEmail) return badRequest(new InvalidParamError("email"));
+
+      return {
+        statusCode: 200,
+        body: null,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError(),
+      };
     }
-
-    const isValidEmail = this.emailValidator.isValid(httpRequest.body.email);
-
-    if (!isValidEmail) return badRequest(new InvalidParamError("email"));
-
-    return {
-      statusCode: 200,
-      body: null,
-    };
   }
 }
