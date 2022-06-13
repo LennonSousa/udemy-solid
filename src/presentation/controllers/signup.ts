9eb9c7572ea24ae40fb84ea3029db1b300b3c57d
John Lennon Sousa Costa <lennonsousa@outlook.com>
2022-06-13 16:23:33 -0300

    refactor: remove code duplication

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -19,17 +19,19 @@ export class SignUpController implements Controller {
         "passwordConfirmation",
       ];
 
+      const { email, password, passwordConfirmation } = httpRequest.body;
+
       // eslint-disable-next-line no-restricted-syntax
       for (const field of requiredFields) {
         if (!httpRequest.body[field])
           return badRequest(new MissingParamError(field));
       }
 
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError("passwordConfirmation"));
       }
 
-      const isValidEmail = this.emailValidator.isValid(httpRequest.body.email);
+      const isValidEmail = this.emailValidator.isValid(email);
 
       if (!isValidEmail) return badRequest(new InvalidParamError("email"));
 
